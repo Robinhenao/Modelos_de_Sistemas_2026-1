2fc7b6820baf3471f41b2b9d4f1e15c7fa3c85d6
Robinson Henao <65988533+Robinhenao@users.noreply.github.com>
2026-05-15 21:53:47 -0500

    Update answer-0343.py

--- myanswers/answer-0343.py
+++ myanswers/answer-0343.py
@@ -28,14 +28,12 @@ def clasificar_biomoleculas(dataset: pd.DataFrame, features: list, target: str) 
     df_limpio['indice_asimetria'] = np.where(denominador > 0, numerador / denominador, 0.0)
 
     le = LabelEncoder()
-    df_limpio['clase_encoded'] = le.fit_transform(df_limpio[target])
-
+    target_vector   = le.fit_transform(df_limpio[target])
     clases_mapeadas = dict(zip(le.classes_, le.transform(le.classes_)))
-    target_vector   = df_limpio['clase_encoded'].values.copy()
 
     features_model = features + ['indice_asimetria']
     X = df_limpio[features_model].values
-    y = df_limpio['clase_encoded'].values
+    y = target_vector.copy()
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.25, random_state=42, stratify=y
@@ -44,15 +42,8 @@ def clasificar_biomoleculas(dataset: pd.DataFrame, features: list, target: str) 
     modelo = RandomForestClassifier(n_estimators=200, random_state=42, n_jobs=-1)
     modelo.fit(X_train, y_train)
 
-    resultado_df = df_limpio.drop(columns=['clase_encoded']).copy()
-    resultado_df['clase_predicha'] = le.inverse_transform(modelo.predict(X))
-    resultado_df['nombre_sistema'] = (
-        resultado_df['id_molecula'].astype(str) + '-' +
-        resultado_df['clase_predicha'].astype(str)
-    )
-
     return {
-        'df_procesado': resultado_df,
+        'df_procesado': df_limpio,
         'target_vector': target_vector,
         'clases_mapeadas': clases_mapeadas
     }
